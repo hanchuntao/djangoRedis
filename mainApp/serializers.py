725035b2c1616dee40f009e26bf1fe64c59f0da3
Chuntao Han <chhan@redhat.com>
2022-11-11 23:52:57 +0800

    django redis use

---
+++ mainApp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Musician
+        fields = '__all__'
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = '__all__'
